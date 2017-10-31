a5bd5566fe9dad23bfc87dd70281ec314360abac
Matt Williams <matt@smbc.edu.au>
2017-10-31 16:22:43 +1100

    Add code to increment click count.
    When case insensitive match is found, increment click count as well.

--- case-insensitive/plugin.php
+++ case-insensitive/plugin.php
@@ -14,6 +14,7 @@ if( !defined( 'YOURLS_ABSPATH' ) ) die();
 // Hook our custom function into the 'add_new_link' filter
 yourls_add_filter( 'shunt_keyword_is_taken', 'insensitive_keyword_is_taken' );
 yourls_add_filter( 'shunt_get_keyword_info', 'insensitive_get_keyword_info' );
+yourls_add_filter( 'shunt_update_clicks', 'insensitive_update_clicks' );
 
 // If the keyword exists, display the long URL in the error message
 function insensitive_keyword_is_taken( $return, $keyword ) {
@@ -65,4 +66,18 @@ function insensitive_get_keyword_infos( $keyword, $use_cache = true ) {
 	}
 
 	return yourls_apply_filter( 'get_keyword_infos', $ydb->get_infos($keyword), $keyword );
+}
+
+ function insensitive_update_clicks( $return, $keyword, $clicks = false ) {
+	global $ydb;
+	$keyword = yourls_sanitize_string( $keyword );
+	$table = YOURLS_DB_TABLE_URL;
+    error_log(var_export($keyword, true));
+	if ( $clicks !== false && is_int( $clicks ) && $clicks >= 0 )
+		$update = $ydb->fetchAffected( "UPDATE `$table` SET `clicks` = :clicks WHERE LOWER(`keyword`) = LOWER(:keyword)", array('clicks' => $clicks, 'keyword' => $keyword) );
+	else
+		$update = $ydb->fetchAffected( "UPDATE `$table` SET `clicks` = clicks + 1 WHERE LOWER(`keyword`) = LOWER(:keyword)", array('keyword' => $keyword) );
+
+	yourls_do_action( 'update_clicks', $keyword, $update, $clicks );
+	return $update;
 }
